9c33a85d647eae90794135fb12a9c8fa5ac857d7
darcangelomauro <pmxmd10@nottingham.ac.uk>
2019-05-08 14:12:46 +0100

    fixed problem in quadratic energy violation check routine

--- tests/leapfrog/quadratic.cpp
+++ tests/leapfrog/quadratic.cpp
@@ -17,30 +17,44 @@ int main()
     gsl_rng_set(engine, time(NULL));
     arma_rng::set_seed(time(NULL)+10);
 
-    const double L = 0.01;
+    //const double L = 1;
 
     // create geometry from input
-    Geom24 G(2, 0, 10, -2.2431);
     
-    double tau = 0.00001;
+    double tau = 0.0001;
 
     ofstream out;
     out.open("quadratic.txt");
     out.precision(16);
 
-    while(tau >= 0.000001)
+    while(tau >= 0.00001)
     {
+        Geom24 G(2, 0, 10, -2.2431);
         G.shuffle();
         G.sample_mom(engine);
         
+        /*
+        vector<double> err;
+        for(int i=0; i<1; ++i)
+        {
+            double Si = G.calculate_S();
+            double Ki = G.calculate_K();
+            G.leapfrog(1000, tau);
+            double Sf = G.calculate_S();
+            double Kf = G.calculate_K();
+            err.push_back(fabs(Sf+Kf-Si-Ki));
+        }
+
+        out << tau << " " << accumulate(err.begin(), err.end(), 0.)/err.size()  << endl;
+        */
+
         double Si = G.calculate_S();
         double Ki = G.calculate_K();
-        G.leapfrog(L/tau, tau);
+        G.leapfrog(1000, tau);
         double Sf = G.calculate_S();
         double Kf = G.calculate_K();
-
-        out << tau << " " << fabs(Sf+Kf-Si-Ki) << endl;
-
+        out << log(tau) << " " << log(fabs(Sf+Kf-Si-Ki)) << endl;
+        
         tau /= 1.1;
     }
 
